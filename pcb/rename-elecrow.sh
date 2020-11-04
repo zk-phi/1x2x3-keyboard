d7adb52cbabe17a2ae90d985fc79c7b4b76956c4
zk-phi <zk-phi@users.noreply.github.com>
2020-11-04 22:18:50 +0900

    Update rename script

--- pcb/rename-elecrow.sh
+++ pcb/rename-elecrow.sh
@@ -1,10 +1,11 @@
-mkdir gerber-elecrow
-mv surfboard-F.Cu.gbr gerber-elecrow/surfboard.GTL
-mv surfboard-B.Cu.gbr gerber-elecrow/surfboard.GBL
-mv surfboard-F.Mask.gbr gerber-elecrow/surfboard.GTS
-mv surfboard-B.Mask.gbr gerber-elecrow/surfboard.GBS
-mv surfboard-F.SilkS.gbr gerber-elecrow/surfboard.GTO
-mv surfboard-B.SilkS.gbr gerber-elecrow/surfboard.GBO
-mv surfboard-PTH.drl gerber-elecrow/surfboard.TXT
-mv surfboard-NPTH.drl gerber-elecrow/surfboard-NPTH.TXT
-mv surfboard-Edge.Cuts.gbr gerber-elecrow/surfboard.GML
+mkdir gerber
+mv surfboard-F.Cu.gbr gerber/surfboard.GTL
+mv surfboard-B.Cu.gbr gerber/surfboard.GBL
+mv surfboard-F.Mask.gbr gerber/surfboard.GTS
+mv surfboard-B.Mask.gbr gerber/surfboard.GBS
+mv surfboard-F.SilkS.gbr gerber/surfboard.GTO
+mv surfboard-B.SilkS.gbr gerber/surfboard.GBO
+mv surfboard-PTH.drl gerber/surfboard.TXT
+mv surfboard-NPTH.drl gerber/surfboard-NPTH.TXT
+mv surfboard-Edge.Cuts.gbr gerber/surfboard.GML
+zip -r gerber gerber
